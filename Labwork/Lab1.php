cf5e3c77652faaeb3c4576f249b31a6ccfb317fe
EC2 Default User <ec2-user@ip-172-31-88-1.ec2.internal>
2019-09-02 06:17:55 +0000

    added more styles

--- Labwork/Lab1.php
+++ Labwork/Lab1.php
@@ -1,16 +1,35 @@
 <!doctype html>
 <html>
     <head>
+        <link href="https://fonts.googleapis.com/css?family=Manjari&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
          <title>Labwork 1:Hello World</title>
          <style>
+             body{
+                 background-color:#E4DFDA;
+             }
+             
              p{
-                 color: red;
+                 color: #4281A4;
+                 font-size: 1.5em;
+                 font-family: 'Mangari', sans-serif;
+             }
+             
+             li{
+                 color: #48A9A6;
+                 font-size:1.5em;
+             }
+             
+             h1{
+                 font-size: 2.5em;
+                 color: #D4B483;
+                 font-family: 'Permanent Marker', cursive;
              }
          </style>
     </head>
     <body>
         <h1>Hi, I'm Ryan Anderson!</h1>
-        <p>Hello everyone, my name is Ryan Anderson. I am a second year Web Developmen student at LBCC. I am an avid gamer (Overwatch, anything on the switch), horror film buff, and all around foodie. I currently work as a kitchen manager for a wood fired pizza catering company, hoping to switch careers to Web Developemnt. </p>
+        <p>Hello everyone, my name is Ryan Anderson. I am a second year Web Developmen student at LBCC. I am an avid gamer (Overwatch, anything on the Switch), horror film buff, and all around foodie. I currently work as a kitchen manager for a wood fired pizza catering company, hoping to switch careers to Web Developemnt. </p>
     
         <p>Some other hobbies I have are:</p>
         
@@ -23,6 +42,7 @@
     
     <?php
         echo"<p>Two of the websites I most frequent are <a href='http://www.theawesomer.com'>TheAwesomer</a> and <a href='http://www.craigslist.com'> Craigslist</p>"
+        echo"<p>I found this process to be quite tedious, actually! I got trapped in a git headache because I missed a step and the changes I made weren't being pushed to my github! I had to re-read the stepts to pushing a change a couple times slowly and once I did them in the correct order everything worked out well.</p>"
         /*lab1.php
         Ryan Anderson
         09/01/19*/
