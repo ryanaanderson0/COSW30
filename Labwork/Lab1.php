dfe4de34c963f825f95ff2409cbec668acb0643e
EC2 Default User <ec2-user@ip-172-31-88-1.ec2.internal>
2019-09-02 06:31:59 +0000

    added a closing link tag to the craigslist link, increased font size of h1

--- Labwork/Lab1.php
+++ Labwork/Lab1.php
@@ -21,7 +21,7 @@
              }
              
              h1{
-                 font-size: 2.5em;
+                 font-size: 4em;
                  color: #D4B483;
                  font-family: 'Permanent Marker', cursive;
              }
@@ -41,7 +41,7 @@
         </ul>
     
     <?php
-        echo"<p>Two of the websites I most frequent are <a href='http://www.theawesomer.com'>TheAwesomer</a> and <a href='http://www.craigslist.com'> Craigslist</p>
+        echo"<p>Two of the websites I most frequent are <a href='http://www.theawesomer.com'>TheAwesomer</a> and <a href='http://www.craigslist.com'> Craigslist</a></p>
         <p>I found this process to be quite tedious, actually! I got trapped in a git headache because I missed a step and the changes I made weren't being pushed to my github! I had to re-read the stepts to pushing a change a couple times slowly and once I did them in the correct order everything worked out well.</p>"
         
         /*lab1.php
