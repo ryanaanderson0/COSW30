ac155ea0d3fb8d735a7ea89344005a07144d07b7
EC2 Default User <ec2-user@ip-172-31-88-1.ec2.internal>
2019-09-10 17:48:01 +0000

    fixed indentation

--- Homework/Homework2/process_contact.php
+++ Homework/Homework2/process_contact.php
@@ -16,8 +16,8 @@
     
     
     echo "<h1> Thank you $first_name for completing the survey! This is the data you gave us: </h1> <br>";
-    echo " First name: $first_name <br><br>";
-    echo " Last name: $last_name <br><br>";
+    echo " First name: $first_name <br>";
+    echo " Last name: $last_name <br>";
     echo " Email: $email <br> ";
     echo " Gender: $gender <br> ";
     echo " Age: $age";
