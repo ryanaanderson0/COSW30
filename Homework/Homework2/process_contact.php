35685165ecf23fbf0790e3c72bd6c1abe0f55f9e
EC2 Default User <ec2-user@ip-172-31-88-1.ec2.internal>
2019-09-10 06:02:53 +0000

    contact.html and process_contact.php added

---
+++ Homework/Homework2/process_contact.php
@@ -0,0 +1,15 @@
+<?php
+
+// Capture data
+
+    $first_name = $_POST ['first_name'];
+    $last_name = $_POST ['last_name'];
+    $email = $_POST ['email'];
+    $gender = $_POST ['gender'];
+    $first_name = $_POST ['first_name'];
+    $age = $_POST ['age'];
+    $interests= $_POST ['interests'];
+    $contribution = $_POST ['quantity'];
+    $mailing_list = $_POST ['mailing_list'];
+
+?>
