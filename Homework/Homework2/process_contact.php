8dcad5a73add0f15369b7b39604c497f3c4a3b5a
EC2 Default User <ec2-user@ip-172-31-88-1.ec2.internal>
2019-09-10 17:08:29 +0000

    interest variable changed

--- Homework/Homework2/process_contact.php
+++ Homework/Homework2/process_contact.php
@@ -7,10 +7,11 @@
     $email = $_POST ['email'];
     $gender = $_POST ['gender'];
     $first_name = $_POST ['first_name'];
-    $age = $_POST ['age'];
+    $age = $_POST ['age1'];
     $interest = $_POST ['interest1'];
     $quantity = $_POST ['quantity'];
     $mailing_list = $_POST ['mailing_list'];
+    $comments = $_POST ['comments'];
     
     
     echo "<h1> Thank you $first_name for completing the survey! This is the data you gave us: </h1> <br>";
@@ -22,7 +23,7 @@
     echo " Interests: $interest <br> ";
     echo " Contribution: $quantity<br> ";
     echo " Mailing list: $mailing_list <br> ";
-    echo " Comments: $comments"
+    echo " Comments: $comments";
     
 
 ?>
