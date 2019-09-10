41d43d4e90874ed42c0e242e5418bddb19608f4d
EC2 Default User <ec2-user@ip-172-31-88-1.ec2.internal>
2019-09-10 06:34:37 +0000

    added more  echo statements

--- Homework/Homework2/process_contact.php
+++ Homework/Homework2/process_contact.php
@@ -13,9 +13,15 @@
     $mailing_list = $_POST ['mailing_list'];
     
     
-    echo "<h1> Thank you $first_name for completing the survey! </h1>";
-    echo " First name: $first_name ";
-    echo " Last name: $last_name ";
+    echo "<h1> Thank you $first_name for completing the survey! This is the data you gave us: </h1> <br>";
+    echo " First name: $first_name <br>";
+    echo " Last name: $last_name <br>";
+    echo " Email: $email <br> ";
+    echo " Gender: $gender <br> ";
+    echo " Age: $age <br> ";
+    echo " Interests: $interests <br> ";
+    echo " Contribution: $quantity <br> ";
+    echo " Mailing list: $mailing_list <br> ";
     
     
 
