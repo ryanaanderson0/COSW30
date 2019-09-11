f67a6562a772146472c68e8fc258118cabeee86f
EC2 Default User <ec2-user@ip-172-31-88-1.ec2.internal>
2019-09-11 01:59:56 +0000

    interest and age  foreach statement

--- Homework/Homework2/process_contact.php
+++ Homework/Homework2/process_contact.php
@@ -25,11 +25,11 @@
     echo "$interest ";
     }
     
-    if(isset($_POST['submit'])){
+    if(isset($_POST['age'])){
 $age = $_POST['age'];  // Storing Selected Value In Variable
-echo "Age: $age";  // Displaying Selected Value
+echo "You have selected :" .$age;  // Displaying Selected Value
 }
-    
+
     echo " Contribution: $quantity<br> ";
     echo " Mailing list: $mailing_list <br> ";
     echo " Comments: $comments";
