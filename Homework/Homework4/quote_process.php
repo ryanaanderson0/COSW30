8b4fc0c94740988eefed359fa63495c4e45041ab
EC2 Default User <ec2-user@ip-172-31-88-1.ec2.internal>
2019-09-25 21:14:44 +0000

    changed name to quote

--- Homework/Homework4/quote_process.php
+++ Homework/Homework4/quote_process.php
@@ -15,7 +15,7 @@
     // echo "<h1> This is your original quote: </h1> <p> $value</p>";
 
     if (!empty($value)) {
-        echo "<h2> Please enter a quote </h2>";
+        alert ("<h2> Please enter a quote </h2>" );
     }
     else  {
         echo "<h2> This is your original quote : $quote </h2> <br>";
@@ -23,6 +23,6 @@
     
    
     echo "<h2> The total word count of your list is: </h2> <br>";
-    echo count($quote) ;
+    echo count($value) ;
 
 ?>
