4831f3cc38244b8ce142aeb760c10d796a00a2e0
EC2 Default User <ec2-user@ip-172-31-88-1.ec2.internal>
2019-09-25 18:14:51 +0000

    added word count

--- Homework/Homework4/quote_process.php
+++ Homework/Homework4/quote_process.php
@@ -7,14 +7,17 @@
 
 // process data
 
-    echo "<h1> This is your original quote: $quote </h1>";
+    echo "<h1> This is your original quote: </h1> <p> $quote </p>";
 
     if (!empty($quote)) {
-        echo "<h2> Your original list is: </h2>";
+        echo "<h2> Your quote as a list: </h2>";
     }
     
     foreach($quote_explode as $value) {
         echo "<p> $value </p>";
     }
+    
+    echo "<h2> The total word count of your list is: </h2>";
+    echo "<p>" .count($quote_explode) ."</p>";
 
 ?>
