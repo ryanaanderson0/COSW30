b27ddf516e2daa00838747dea31001b8396312b4
EC2 Default User <ec2-user@ip-172-31-88-1.ec2.internal>
2019-09-25 05:45:37 +0000

    added Homework4 html and php files

---
+++ Homework/Homework4/quote_process.php
@@ -0,0 +1,18 @@
+<?php
+
+// capture data
+
+    $quote = $_POST ['quote'];
+
+
+// process data
+
+    if (!empty)$quote)) {
+        echo "<h2> Your original list is: </h2>";
+    }
+    
+    foreach($quote_explode as $value) {
+        echo "<p> $value </p>";
+    }
+
+?>
