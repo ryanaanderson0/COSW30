d3bf0b419286421e889a8332c93eef9bccca960b
ryanaanderson0 <ryanaanderson0@gmail.com>
2019-11-06 02:16:42 +0000

    added insert query

--- Homework/Homework10/crud.php
+++ Homework/Homework10/crud.php
@@ -11,7 +11,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 *   QUERY THE DATABASE AND STORE ALL USERS INTO A VARIABLE
 */
 // Create your query
-$query = 'SELECT * FROM USER';
+$query = "INSERT INTO USER_ANDERSON (first_name, last_name, email, password)
+        VALUES ('$firstName', '$lastName', '$email', '$password')
+
+        SELECT * FROM USER_ANDERSON";
 // Run your query
 $result = mysqli_query($connection, $query);
 // Check if the database returned anything
@@ -21,6 +24,7 @@ if($result) {
     $rows = mysqli_fetch_all($result);
 } else {
     // Output an error
+    echo "There has been an error connecting to the database";
 }
 ?>
 
@@ -33,19 +37,19 @@ if($result) {
     <h1>Create a New User</h1>
     <form action="crud.php" method="POST">
         <label for="first_name">First Name</label>
-        <input type="text" id="first_name" name="first_name"><br>
+        <input type="text" id="first_name" name="first_name" required><br>
 
         <label for="last_name">Last Name</label>
-        <input type="text" id="last_name" name="last_name"><br>
+        <input type="text" id="last_name" name="last_name" required><br>
 
         <label for="email">Email</label>
-        <input type="email" id="email" name="email"><br>
+        <input type="email" id="email" name="email" required><br>
 
         <label for="password">Password</label>
-        <input type="password" id="password" name="password"><br>
+        <input type="password" id="password" name="password" required><br>
 
         <label for="password">Retype Password</label>
-        <input type="password" id="password" name="password"><br>
+        <input type="password" id="password" name="password" required><br>
 
         <!--Add a second password input so the user has to retype their password -->
 
@@ -63,12 +67,19 @@ if($result) {
             </tr>
         </thead>
         <tbody>
-            <?php // You will be adding a forEach loop here to output the users ?>
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+            <?php // You will be adding a forEach loop here to output the users
+            foreach($rows as $row){
+                echo "<td>".$row['first_name']. "</td>";
+                echo "<td>".$row['last_name']. "</td>";
+                echo "<td>".$row['email']. "</td>";
+                echo "<td>".$row['password']. "</td>";
+
+            }
+            else {
+                echo $rows;
+            }
+            ?>
             </tr>
         </tbody>
     </table>
