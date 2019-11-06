e8fcd0f4da924f78dec66e5a576e9ba32ecc75a9
ryanaanderson0 <ryanaanderson0@gmail.com>
2019-11-06 01:50:30 +0000

    added homework10 folder

---
+++ Homework/Homework10/crud.php
@@ -0,0 +1,76 @@
+<?php
+// Add the database connection
+include('database.php');
+/*
+*   CHECK IF THE FORM HAS BEEN SUBMITTED AND INSERT
+*   NEW USER INTO THE DATABASE
+*/
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+}
+/*
+*   QUERY THE DATABASE AND STORE ALL USERS INTO A VARIABLE
+*/
+// Create your query
+$query = 'SELECT * FROM USER';
+// Run your query
+$result = mysqli_query($connection, $query);
+// Check if the database returned anything
+if($result) {
+    // If the database query was successful, store
+    // the array of users into a variable
+    $rows = mysqli_fetch_all($result);
+} else {
+    // Output an error
+}
+?>
+
+<!doctype html>
+<html>
+<head>
+    <title>My First CRUD</title>
+</head>
+<body>
+    <h1>Create a New User</h1>
+    <form action="crud.php" method="POST">
+        <label for="first_name">First Name</label>
+        <input type="text" id="first_name" name="first_name"><br>
+
+        <label for="last_name">Last Name</label>
+        <input type="text" id="last_name" name="last_name"><br>
+
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email"><br>
+
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password"><br>
+
+        <label for="password">Retype Password</label>
+        <input type="password" id="password" name="password"><br>
+
+        <!--Add a second password input so the user has to retype their password -->
+
+        <button>Register</button>
+    </form>
+
+    <h2>Output a List of Users</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Password</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php // You will be adding a forEach loop here to output the users ?>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
