fd8278ab648f62692f1d5a9df51f9cbaaff7560e
ryanaanderson0 <ryanaanderson0@gmail.com>
2019-11-07 04:02:02 +0000

    added update.php file and connected to database on crud.php

--- Homework/Homework10/crud.php
+++ Homework/Homework10/crud.php
@@ -16,28 +16,56 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+//initialize error array
 
-$insert_query = "INSERT INTO USER_ANDERSON (first_name, last_name, email, password)
+    $error = [];
+
+//Verify that the form is filled out/not empty
+
+    if(empty($_POST['first_name'])) {
+        $error[] = 'You forgot to enter your first name!';
+    }
+    else {
+        $first_name = trim($_POST['first_name']);
+    }
+
+//check if the passwords match
+
+    if (!empty($_POST['password'])) {
+        if ($_POST['password'] != $_POST['confirm_password']) {
+            $error[] = 'Your passwords do not match!';
+        }
+        else{
+        $password = trim($_POST['password']);
+        }
+    }else{
+        $error[] = 'You forgot to enter a password!';
+    }
+
+
+    $insert_query = "INSERT INTO USER_ANDERSON (first_name, last_name, email, password)
         VALUES ('$first_name', '$last_name', '$email', '$password')
         ";
 
-$result = mysqli_query($connection, $insert_query);
-/*
-*   QUERY THE DATABASE AND STORE ALL USERS INTO A VARIABLE
-*/
-// Create your query
-$query = "SELECT * FROM USER_ANDERSON;
-        ";
-// Run your query
-$result = mysqli_query($connection, $query);
-// Check if the database returned anything
-if($result) {
-    // If the database query was successful, store
-    // the array of users into a variable
-    $rows = mysqli_fetch_all($result);
-} else {
-    // Output an error
-}
+    $result = mysqli_query($connection, $insert_query);
+    /*
+    *   QUERY THE DATABASE AND STORE ALL USERS INTO A VARIABLE
+    */
+    // Create your query
+    $query = "SELECT * FROM USER_ANDERSON;
+            ";
+    // Run your query
+    $result = mysqli_query($connection, $query);
+    // Check if the database returned anything
+
+    if($result) {
+        // If the database query was successful, store
+        // the array of users into a variable
+        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else {
+        // Output an error
+        $error[] = 'Woops, did not enter data into the database!';
+    }
 }
 ?>
 
@@ -50,19 +78,19 @@ if($result) {
     <h1>Create a New User</h1>
     <form action="crud.php" method="POST">
         <label for="first_name">First Name</label>
-        <input type="text" id="first_name" name="first_name" required><br>
+        <input type="text" id="first_name" name="first_name"><br>
 
         <label for="last_name">Last Name</label>
-        <input type="text" id="last_name" name="last_name" required><br>
+        <input type="text" id="last_name" name="last_name"><br>
 
         <label for="email">Email</label>
-        <input type="email" id="email" name="email" required><br>
+        <input type="email" id="email" name="email"><br>
 
         <label for="password">Password</label>
-        <input type="password" id="password" name="password" required><br>
+        <input type="password" id="password" name="password"><br>
 
         <label for="password">Retype Password</label>
-        <input type="password" id="password" name="password" required><br>
+        <input type="password" id="comfirm_password" name="confirm_password"><br>
 
         <!--Add a second password input so the user has to retype their password -->
 
@@ -83,10 +111,13 @@ if($result) {
 
             <?php // You will be adding a forEach loop here to output the users
             foreach($rows as $row){
-                echo "<tr><td>".$row['first_name']. "</td>";
-                echo "<td>".$row['last_name']. "</td>";
-                echo "<td>".$row['email']. "</td>";
-                echo "<td>".$row['password']. "</td></tr>";
+                echo '<tr>
+                        <td>'.$row['first_name']. '</td>
+                        <td>'.$row['last_name']. '</td>
+                        <td>'.$row['email']. '</td>
+                        <td>'.$row['password']. '</td>
+                        <td><a href="update.php?id='.$row['user_id'].'">Edit</a></td>
+                    </tr>';
 
             }
             ?>
